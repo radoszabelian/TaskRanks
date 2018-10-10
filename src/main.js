896868226a177dc4c8501a1a14f8bad97985872d
Belian Radosza <radoszabelian@gmail.com>
2018-10-10 12:53:01 +0200

    Data writing now live, dependencies changed to CDN

--- src/main.js
+++ src/main.js
@@ -1,3 +1,66 @@
+var uiController = (function (_blModule) {
+    let _module = {};
+
+    function generateTierStripesTo(element) {
+        for (let i = 1; i < _blModule.getCurrentTier(); i++) {
+            let tierStripeTemplate = document.querySelector("#_templates > ._tier_star_template").cloneNode(true);
+            element.appendChild(tierStripeTemplate);
+        }
+    }
+
+    _module.addCompletedTask = (event) => {
+        let inputElement = document.querySelector("#_edit-input-add");
+        if (Number.isInteger(parseInt(inputElement.value)) && parseInt(inputElement.value) >= 0) {
+            _blModule.addCompletedTasks(parseInt(inputElement.value)).then(() => {
+                _blModule.refreshData().then(() => {
+                    _module.refreshUi();
+                })
+            });
+        }
+        inputElement.value = 0;
+    }
+
+    _module.removeCompletedTask = (event) => {
+        let inputElement = document.querySelector("#_edit-input-remove");
+        if (Number.isInteger(parseInt(inputElement.value)) && parseInt(inputElement.value) >= 0) {
+            _blModule.removeCompletedTasks(parseInt(inputElement.value)).then(() => {
+                _blModule.refreshData().then(() => {
+                    _module.refreshUi();
+                })
+            });
+
+        }
+        inputElement.value = 0;
+    }
+
+    _module.refreshUi = () => {
+        _blModule.refreshData().then(() => {
+            let completedTasksElement = document.getElementById("_completed-tasks-count");
+            let progressBarElement = document.getElementById("_progress-bar");
+            let progressTitleElement = document.getElementById("_progress-title");
+            let rankImageElement = document.getElementById("_rank-image");
+            let tierTitleElement = document.getElementById("_tier_title");
+            let tierStripesContainerElement = document.getElementById("_tier-stripe-container");
+
+            completedTasksElement.innerHTML = taskRanksBL.getCompletedTasks();
+            progressBarElement.setAttribute("style", `width: ${taskRanksBL.getProgressPercentage()}%`);
+            progressTitleElement.innerHTML = `LEVEL ${taskRanksBL.getCurrentRank()}`;
+            rankImageElement.setAttribute("src", `assets/ranks/hu/${taskRanksBL.getCurrentRank()}.svg`)
+            tierTitleElement.innerHTML = taskRanksBL.getCurrentTier();
+            generateTierStripesTo(tierStripesContainerElement);
+        });
+    };
+
+    _module.toggleEditUi = (show = false) => {
+        let addUiElement = document.querySelector("#_left_edit_container");
+        let removeUiElement = document.querySelector("#_right_edit_container");
+        addUiElement.setAttribute("style", show ? "" : "display: none !important");
+        removeUiElement.setAttribute("style", show ? "" : "display: none !important");
+    }
+
+    return _module;
+});
+
 var taskRanksBL = (function (_serviceModule) {
     let _module = {};
 
@@ -32,7 +95,11 @@ var taskRanksBL = (function (_serviceModule) {
     }
 
     _module.addCompletedTasks = (howMany) => {
-        _serviceModule.setCompletedTasksNumber(completedTasks + howMany);
+        return _serviceModule.setCompletedTasksNumber(completedTasks + howMany);
+    }
+
+    _module.removeCompletedTasks = (howMany) => {
+        return _serviceModule.setCompletedTasksNumber(completedTasks - howMany);
     }
 
     function computeRankStates() {
@@ -42,60 +109,6 @@ var taskRanksBL = (function (_serviceModule) {
 
     _module.getProgressPercentage = () => {
         return Math.round((((completedTasks % taskPerRank) / taskPerRank) * 100));
-    }
-
-    return _module;
-});
-
-var uiController = (function (_blModule) {
-    let _module = {};
-
-    function generateTierStripesTo(element) {
-        for (let i = 1; i < _blModule.getCurrentTier(); i++) {
-            let tierStripeTemplate = document.querySelector("#_templates > ._tier_star_template").cloneNode(true);
-            element.appendChild(tierStripeTemplate);
-        }
-    }
-
-    _module.addCompletedTask = (event) => {
-        let inputElement = document.querySelector("#_edit-input-add");
-        if (Number.isInteger(parseInt(inputElement.value)) && parseInt(inputElement.value) >= 0) {
-            _blModule.addCompletedTasks(parseInt(inputElement.value));
-        }
-        inputElement.value = 0;
-    }
-
-    _module.removeCompletedTask = (event) => {
-        let inputElement = document.querySelector("#_edit-input-remove");
-        if (Number.isInteger(parseInt(inputElement.value)) && parseInt(inputElement.value) >= 0) {
-            //change data in db
-        }
-        inputElement.value = 0;
-    }
-
-    _module.refreshUi = () => {
-        _blModule.refreshData().then(() => {
-            let completedTasksElement = document.getElementById("_completed-tasks-count");
-            let progressBarElement = document.getElementById("_progress-bar");
-            let progressTitleElement = document.getElementById("_progress-title");
-            let rankImageElement = document.getElementById("_rank-image");
-            let tierTitleElement = document.getElementById("_tier_title");
-            let tierStripesContainerElement = document.getElementById("_tier-stripe-container");
-
-            completedTasksElement.innerHTML = taskRanksBL.getCompletedTasks();
-            progressBarElement.setAttribute("style", `width: ${taskRanksBL.getProgressPercentage()}%`);
-            progressTitleElement.innerHTML = `LEVEL ${taskRanksBL.getCurrentRank()}`;
-            rankImageElement.setAttribute("src", `assets/ranks/hu/${taskRanksBL.getCurrentRank()}.svg`)
-            tierTitleElement.innerHTML = taskRanksBL.getCurrentTier();
-            generateTierStripesTo(tierStripesContainerElement);
-        });
-    };
-
-    _module.toggleEditUi = (show = false) => {
-        let addUiElement = document.querySelector("#_left_edit_container");
-        let removeUiElement = document.querySelector("#_right_edit_container");
-        addUiElement.setAttribute("style", show ? "" : "display: none !important");
-        removeUiElement.setAttribute("style", show ? "" : "display: none !important");
     }
 
     return _module;
@@ -123,14 +136,21 @@ var dataService = (function (_db) {
     };
 
     _module.setCompletedTasksNumber = (completedTasksNumber) => {
-        if (Number.isInteger(completedTasksNumber)) {
-            db.ref(`-${collectionName}/-${dataId}/completedTasks`).set(completedTasksNumber)
-            console.log("Data added");
-        } else {
-            console.error("Data adding failed!!!");
-        }
+        return new Promise((resolve, reject) => {
+            if (Number.isInteger(completedTasksNumber)) {
+                db.collection(collectionName).doc(dataId).set({
+                    completedTasks: completedTasksNumber
+                }, { merge: true })
+                    .then(() => {
+                        resolve();
+                    }, () => {
+                        reject();
+                    });
+            } else {
+                reject();
+            }
+        })
     }
-
 
     return _module;
 });
